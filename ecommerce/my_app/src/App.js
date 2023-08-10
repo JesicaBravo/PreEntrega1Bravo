ef0e58cf54bba8c5f39b4571b9406192e8ed3c0c
Jesica Bravo <jesicabravoa@gmail.com>
2023-08-10 00:03:29 -0300

    primera preentrega

---
+++ ecommerce/my_app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { ItemListContainer } from './components/ItemsListContainer';
+import { NavBar } from './components/NavBar';
+
+function App() {
+  return (
+    <>
+
+<ItemListContainer></ItemListContainer>
+</>
+  );
+}
+
+export default App;
